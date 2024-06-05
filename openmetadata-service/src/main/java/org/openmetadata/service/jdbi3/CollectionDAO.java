312526949b7e5ce2eb3b4e395487759c96e47b12
ulixius9 <mayursingal9@gmail.com>
2024-06-05 11:10:59 +0530

    Data Insights Refactor

--- openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/CollectionDAO.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/CollectionDAO.java
@@ -64,6 +64,7 @@ import org.openmetadata.schema.auth.RefreshToken;
 import org.openmetadata.schema.auth.TokenType;
 import org.openmetadata.schema.dataInsight.DataInsightChart;
 import org.openmetadata.schema.dataInsight.kpi.Kpi;
+import org.openmetadata.schema.dataInsightNew.DIChart;
 import org.openmetadata.schema.email.SmtpSettings;
 import org.openmetadata.schema.entities.docStore.Document;
 import org.openmetadata.schema.entity.Bot;
@@ -307,6 +308,9 @@ public interface CollectionDAO {
 
   @CreateSqlObject
   WebAnalyticEventDAO webAnalyticEventDAO();
+
+  @CreateSqlObject
+  DIChartDao diChartDAO();
 
   @CreateSqlObject
   DataInsightChartDAO dataInsightChartDAO();
@@ -3710,6 +3714,23 @@ public interface CollectionDAO {
     @Override
     default Class<WebAnalyticEvent> getEntityClass() {
       return WebAnalyticEvent.class;
+    }
+
+    @Override
+    default String getNameHashColumn() {
+      return "fqnHash";
+    }
+  }
+
+  interface DIChartDao extends EntityDAO<DIChart> {
+    @Override
+    default String getTableName() {
+      return "di_chart_entity";
+    }
+
+    @Override
+    default Class<DIChart> getEntityClass() {
+      return DIChart.class;
     }
 
     @Override
